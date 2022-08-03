29d055f70c442ad0cf528ea4ea3a65a2111e35f9
YI CHUN CHO <happysorda@gmail.com>
2022-08-02 22:43:35 -0700

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,7 +6,7 @@ const { User } = require('../../db/models');
 const router = express.Router();
 
 // log in
-router.post('/', async (req, res, next) => {
+router.post( '/', async (req, res, next) => {
 
     const { credential, password } = req.body;
   
@@ -24,6 +24,14 @@ router.post('/', async (req, res, next) => {
   
     return res.json({ user });
 });
+
+// Log out
+router.delete( '/', (_req, res) => {
+    
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+});
+  
 
 
 
